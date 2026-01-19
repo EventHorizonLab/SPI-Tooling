8aa16d6ce626134ddda328930061504fdfa540a5
Liam <46268350+LMLiam@users.noreply.github.com>
2026-01-19 20:04:46 +0000

    Make Gradle runner resilient to local JDK setup

--- modules/annotations/src/main/kotlin/com/github/eventhorizonlab/spi/ServiceScheme.kt
+++ modules/annotations/src/main/kotlin/com/github/eventhorizonlab/spi/ServiceScheme.kt
@@ -10,7 +10,7 @@ import kotlin.reflect.KClass
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class ServiceProvider(
-    vararg val value: KClass<*>,
+    vararg val value: KClass<*> = [],
 )
 
 /**
